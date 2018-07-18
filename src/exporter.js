252d9f185bde399efaadff75986c18aeeb0fe092
Takuya Matsuyama <nora@odoruinu.net>
2018-07-18 16:02:23 +0900

    fix(export): Fix file name

--- src/exporter.js
+++ src/exporter.js
@@ -72,7 +72,9 @@ export async function exportBook(parentDir, book) {
 export async function exportNote(note, pathToSave, fileName) {
   if (note.body) {
     const datestr = moment(note.createdAt).format('YYYYMMDD')
-    fileName = fileName || sanitize(datestr + '-' + note.title + '-' + note._id) + '.md'
+    fileName =
+      fileName ||
+      sanitize(datestr + '-' + note.title + '-' + note._id.substr(5)) + '.md'
     const filePath = path.join(pathToSave, fileName)
     let body = '# ' + note.title + '\n\n' + note.body
 
